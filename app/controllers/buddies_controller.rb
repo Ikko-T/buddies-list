91071a7f543ec0bfde8dec6f39c2f57a7f3b76e5
Ikko-T <skyblueboy32@gmail.com>
2022-02-09 09:19:15 +0900

    Style root page

---
+++ app/controllers/buddies_controller.rb
@@ -0,0 +1,20 @@
+class BuddiesController < ApplicationController
+  def index
+    @buddies = Buddy.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
